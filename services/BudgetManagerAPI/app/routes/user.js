3599f874359f251b0ef292a9e248abf621393f91
Daley <daley.mottley@hotmail.com>
2018-10-30 23:57:02 +0000

    new models added

--- services/BudgetManagerAPI/app/routes/user.js
+++ services/BudgetManagerAPI/app/routes/user.js
@@ -5,22 +5,7 @@ const passport  = require('passport'),
 module.exports = (app) => {
     const api = app.BudgetManagerAPI.app.api.user;
     
-    app.route('/api/v1/setup')
-        .post(api.setup(models.User))
-        
-    app.route('/api/v1/users')
-        .get(passport.authenticate('jwt', config.session), api.index(models.User, app.get('budgetsecret')));
-        
     app.route('/api/v1/signup')
         .post(api.signup(models.User));
-<<<<<<< HEAD
-
-} // before git revert
-=======
 }
-<<<<<<< HEAD
-=======
 
-// Testing git
->>>>>>> parent of 53da61c... Slight changes
->>>>>>> parent of 2c8f2df... after revert
